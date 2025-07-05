c959d9a16c309522b3900678354e10f75e6eb9e8
tslime <taha.slime@gmail.com>
2025-07-05 14:47:30 -0400

    message handler adapted to NIO almost complete

--- BasicClientServerProgram/Java/Msghandler.java
+++ BasicClientServerProgram/Java/Msghandler.java
@@ -4,15 +4,30 @@ import java.util.Scanner;
 //Network libraries
 import java.net.Socket;
 import java.net.ServerSocket;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.*;
 
 
 class Msghandler{
     
+
+    ByteBuffer buffer;
+
+    /*
     InputStream in;
     OutputStream out;
     BufferedReader r;
     PrintWriter w;
+    */
 
+      public Msghandler(int size){
+            this.buffer = ByteBuffer.allocate(size);
+     
+    }
+
+    /*
     public Msghandler(Socket c){
     
         try{
@@ -23,8 +38,29 @@ class Msghandler{
         }catch(IOException e){
             System.out.println("Error during the creation of message pipeline");
         }
-    }
+    }*/
 
+   public void receive_message(SocketChannel s){
+    int msg_length;
+    String msg = "";
+
+    try{
+        
+        while( (msg_length = s.read(this.buffer)) > 0 ){
+            this.buffer.flip();
+            msg += StandardCharsets.UTF_8.decode(this.buffer).toString();
+        
+        }
+        
+
+        System.out.println("Client says: "+msg);
+        }catch(IOException e){
+            System.out.println("Error processing message");
+        }
+
+   }
+
+   /*
     public boolean receive_message(BufferedReader rc,long client_num){
         
         try{
@@ -42,7 +78,6 @@ class Msghandler{
             return false;
         }
     }
-
     public void send_message(PrintWriter wr){
 
         wr.println("Message received");
@@ -51,13 +86,13 @@ class Msghandler{
         System.out.println("Error during message transmission");
         
 
-    }
+    }*/
 
-    /*
+    
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("test");
-    }*/
+    }
 }
