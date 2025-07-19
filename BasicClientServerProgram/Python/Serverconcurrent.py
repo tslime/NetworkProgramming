d26ac12cc906ec141c770d9f452e1d2a0d58bfb7
tslime <taha.slime@gmail.com>
2025-07-19 00:51:38 -0400

    concurrent server\client with asyncio almost done

--- BasicClientServerProgram/Python/Serverconcurrent.py
+++ BasicClientServerProgram/Python/Serverconcurrent.py
@@ -3,7 +3,29 @@ import sys
 import asyncio
 
 from inc.Msghandler import Msghandler
+from functools import partial
 
+SERVER_ADDR = "192.168.2.57"
+PORT = 6379
+c_num = 0
 
+async def handle_client(r,w):
+    global c_num 
+    c_num = c_num + 1
+    print("client connected....\n")
 
-print("test \n")
+    while True:
+        msg = await r.readline()
+        print("client ",c_num," says: ",msg.decode())
+        w.write("Message received \n".encode())
+        await w.drain()
+    
+    w.close()
+
+async def main():
+    c_num = 0
+    server = await asyncio.start_server(handle_client,SERVER_ADDR,PORT)
+    await server.serve_forever()
+    
+    
+asyncio.run(main())
