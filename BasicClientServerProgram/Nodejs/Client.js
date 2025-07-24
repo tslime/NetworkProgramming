5010c01c7cfe8bc9c411d1c5c0867b464a199226
tslime <taha.slime@gmail.com>
2025-07-24 00:38:24 -0400

    node js client\server implemetation tuned slighlty

--- BasicClientServerProgram/Nodejs/Client.js
+++ BasicClientServerProgram/Nodejs/Client.js
@@ -4,15 +4,14 @@ const readline = require('readline')
 const PORT = 6379;
 const HOST = '192.168.2.57';
 
-const client = net.createConnection({port:PORT,host:HOST},function(){
-    s.prompt();
-});
-
-
 const s = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     prompt: "Write the message you want to send \n"
+});
+
+const client = net.createConnection({port:PORT,host:HOST},function(){
+    s.prompt();
 });
 
 
