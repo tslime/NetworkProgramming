b0290913bc8c1abda19e37b02ea02ffa590ddf47
tslime <taha.slime@gmail.com>
2026-01-29 12:52:43 -0500

    Rust basic client server complete

---
+++ client-server-programs/Rust/basic-client-server/src/main.rs
@@ -0,0 +1,20 @@
+use std::net::{TcpListener,TcpStream};
+use std::io::{Read,Write};
+
+mod utils;
+use utils::handle_client::handle_client;
+
+
+fn main(){
+
+    let server_socket = TcpListener::bind("192.168.2.57:2222").unwrap();
+    println!("Server running.... \n");
+    let (mut stream,address) = server_socket.accept().unwrap();
+    
+    
+    loop{
+        handle_client(&mut stream);
+        stream.write_all(b"message received! \n\n").unwrap();
+    };
+  
+}
