ba154405cee27458ea607e79254fac7c135455f2
tslime <taha.slime@gmail.com>
2026-01-30 23:58:29 -0500

    concurrent server with thread halfway

--- client-server-programs/Rust/concurrent-server-thread/src/main.rs
+++ client-server-programs/Rust/concurrent-server-thread/src/main.rs
@@ -7,7 +7,14 @@ use std::thread;
 fn main() {
 
     let concurrent_server_socket = TcpListener::bind("192.168.2.57:2222").unwrap();
-    println!("Concurrent successfully started and running....");
+    println!("Concurrent server successfully started and running....");
+
+    for incoming_request in concurrent_server_socket.incoming(){
+       let mut client_stream = incoming_request.unwrap();
+       thread::spawn( move || {
+        handle_client(client_stream)
+       });
+    }
 
     
 }
