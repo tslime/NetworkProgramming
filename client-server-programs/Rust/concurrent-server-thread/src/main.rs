c66f391a6da6ae111aeb15187fe480bb415f19fc
tslime <taha.slime@gmail.com>
2026-02-02 23:13:16 -0500

    error handling and logging added to concurrent thread implementation

--- client-server-programs/Rust/concurrent-server-thread/src/main.rs
+++ client-server-programs/Rust/concurrent-server-thread/src/main.rs
@@ -1,19 +1,32 @@
 mod utils;
 use utils::handle_client::handle_client;
 
+use clap::Parser;
 use std::net::TcpListener;
 use std::thread;
+use std::error;
+use log::{error,info,warn,debug,trace};
 
-fn main() {
 
-    let concurrent_server_socket = TcpListener::bind("192.168.2.57:2222").unwrap();
-    println!("Concurrent server successfully started and running....");
+#[derive(Parser)]
+struct Arguments{
+    #[clap(long)]
+    ip_and_port: String,
+}
+
+fn main() -> Result<(),Box<dyn error::Error>> {
+    env_logger.init();
+
+    let concurrent_server_socket = TcpListener::bind("192.168.2.57:2222").map_err(|e| {error!("Failed to bind {}", e);e})?;
+    info!("Concurrent server successfully started and running....");
 
     for incoming_request in concurrent_server_socket.incoming(){
-       let mut client_stream = incoming_request.unwrap();
+       let mut client_stream = incoming_request.map_err(|e| {error!("Failed to accept client {}", e);e})?;
        thread::spawn( move || {
-        handle_client(client_stream)
+        handle_client(client_stream)?;
        });
     }
+
+    Ok(())
    
 }
--- client-server-programs/Rust/concurrent-server-thread/src/utils/handle_client.rs
+++ client-server-programs/Rust/concurrent-server-thread/src/utils/handle_client.rs
@@ -1,13 +1,21 @@
 use std::net::{TcpListener,TcpStream};
 use std::io::{Read,Write};
+use std::error;
+use log::{error,info,warn,debug,trace};
 
-pub fn handle_client(mut client_stream: TcpStream){
+pub fn handle_client(mut client_stream: TcpStream) -> Result<(),Box<dyn error::Error>> {
     let mut buffer = [0;512];
 
     loop{
-        let bytes_read = client_stream.read(&mut buffer).unwrap();
+        let bytes_read = client_stream.read(&mut buffer).map_err(|e| {error!("Failed to read data {}", e);e})?;
+         if bytes_read == 0 {
+            break;
+        }
+
         let message = String::from_utf8_lossy(&buffer[..bytes_read]);
         println!("client message: {}",message);
-        client_stream.write_all(b"Message received\n").unwrap();
-    };
+        client_stream.write_all(b"Message received\n").map_err(|e| {error!("Failed to send data {}", e);e})?;
+    }
+
+    Ok(())
 }
